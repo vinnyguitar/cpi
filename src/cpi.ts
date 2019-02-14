55c7e7bf8b9af876142cc0aebfa671825b9a8357
liwei <liwei@youzan.com>
2019-02-14 10:27:24 +0800

    patton

--- src/cpi.ts
+++ src/cpi.ts
@@ -1,5 +1,6 @@
 import * as assert from 'assert';
 import * as fs from 'fs';
+import * as fse from 'fs-extra';
 import * as glob from 'glob';
 import * as path from 'path';
 import * as ts from 'typescript';
@@ -10,6 +11,7 @@ assert(configJson, 'cpiconfig.json must have!');
 const config = JSON.parse(configJson);
 assert(config.rootDir, 'cpiconfig.rootDir must have!');
 assert(config.outDir, 'cpiconfig.outDit must have!');
+assert(config.files, 'cpiconfig.files must have!');
 
 const compilerOptions = {
     module: ts.ModuleKind.CommonJS,
@@ -24,20 +26,41 @@ const compilerOptions = {
     rootDir: config.rootDir,
 };
 
-const files = glob.sync('**/*.controller.ts', { cwd: config.rootDir })
-    .map((f: string) => path.join(config.rootDir, f));
+const files = config.files
+    .map((patten: string) => glob.sync(patten, { cwd: config.rootDir }))
+    .reduce((acc, cur) => acc.concat(cur), []);
 
 const compilerHost = ts.createCompilerHost(compilerOptions);
-const program = ts.createProgram(files, compilerOptions, compilerHost);
+const program = ts.createProgram(files.map((f: string) => path.join(config.rootDir, f)), compilerOptions, compilerHost);
 
-program.emit(undefined, undefined, undefined, undefined, {
-    before: [
-        transformer,
-    ],
-});
+program.emit(undefined, (
+    fileName,
+    data,
+    writeByteOrderMark,
+    onError,
+    sourceFiles) => {
+    if (!isOutput(fileName)) {
+        return;
+    }
+    compilerHost.writeFile(fileName, data, writeByteOrderMark, onError, sourceFiles);
+}, undefined, undefined, {
+        before: [
+            transformer,
+        ],
+    });
 const pkg = {
     name: config.name,
     version: config.version,
     description: config.description,
 };
-fs.writeFileSync(path.join(config.outDir, 'package.json'), JSON.stringify(pkg, undefined, '  '));
+fse.outputFileSync(path.join(config.outDir, 'package.json'), JSON.stringify(pkg, undefined, '  '));
+
+function isOutput(file: string) {
+    const list = files.slice(0);
+    while (list.length) {
+        const item = list.pop();
+        if (file.replace(/(\.d)?\.(ts|js)$/, '.ts').endsWith(item)) {
+            return true;
+        }
+    }
+}
